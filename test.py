4c85c01b2f084bdf417b74f20fb53a934bbfc50d
enyu yu <enyu yu>
2016-08-29 22:05:28 +0800

    python

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
+# -*- coding: utf-8 -*-
 __author__ = 'yuenyu111'
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+# 提取一条微博信息
+# output = open('F:/Document/MicroBlogPredict/weibo3903741306239294.txt', 'w+')
+# with open('F:/Document/MicroBlogPredict/trainRepost.txt', 'r') as f:
+#     for position, line in enumerate(f):
+#         t= line.strip().split('\001')
+#         print t[0]
+#         if t[0] == '3903741306239294':
+#             output.writelines(line)
+#             output.writelines('\n')
+# output.close()
+
+#一条微博信息的传播深度和传播广度与时间曲线
+
+
