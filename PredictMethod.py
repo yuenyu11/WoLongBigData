a36cc5f11fca24c5bcb2d067070476c322b7f97c
yuenyu1 <943530714@qq.com>
2016-08-31 21:27:23 +0800

    python

---
+++ PredictMethod.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__author__ = 'yuenyu111'
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+
--- test.py
+++ test.py
@@ -9,57 +9,82 @@ def MissInformation(G,mainNode):#转发链断裂处理
         if G.in_degree(i)==0 and i !=mainNode:
             G.add_edge(mainNode,i)
     return G
-def OneMicroblog(mainNode):# 提取一条微博信息
-    output = open('F:/github/WoLongBigData/DataManager/weibo'+mainNode+'.txt', 'w+')
+def OneMicroblog(Wid):# 提取一条微博信息
+    output = open('F:/github/WoLongBigData/DataManager/weibo'+Wid+'.txt', 'w+')
     with open('F:/Document/MicroBlogPredict/trainRepost.txt', 'r') as f:
         for position, line in enumerate(f):
             t= line.strip().split('\001')
             print t[0]
-            if t[0] == mainNode:
+            if t[0] == Wid:
                 output.writelines(line)
     output.close()
 
-# #一条微博信息的转发网络
-# G = nx.DiGraph()
-# with open('F:/github/WoLongBigData/DataManager/weibo3794305741726764.txt', 'r') as f:
-#     for position, line in enumerate(f):
-#         t= line.strip().split('\001')
-#         G.add_node(t[2],time=t[3],content=t[4])
-#         G.add_edge(t[1],t[2])
-# G = MissInformation(G,'2724513')
-# nx.draw(G)
-# plt.savefig("Picture/weibo3794305741726764.png")
-# plt.show()
+def RTnet(Wid,mainNode):#一条微博信息的转发网络
+    G = nx.DiGraph()
+    with open('F:/github/WoLongBigData/DataManager/weibo'+Wid+'.txt', 'r') as f:
+        for position, line in enumerate(f):
+            t= line.strip().split('\001')
+            G.add_node(t[2],time=t[3],content=t[4])
+            G.add_edge(t[1],t[2])
+    G = MissInformation(G,mainNode)
+    nx.draw(G)
+    plt.savefig('Picture/weibo'+Wid+'.png')
+    plt.show()
 
-#一条微博信息的传播深度和传播广度与时间曲线
+def RTWidth(G,Wid):#一条微博信息的累计传播广度
+    width=['']*300
+    time = nx.get_node_attributes(G,'time')
+    for i in time:
+        if time[i]/900 < 288:
+            width[(time[i]/900)+1]=width[(time[i]/900)+1]+1
+    for i in range(width.__len__()-1):
+        width[i+1] =width[i+1]+width[i]
+    x=range(300)
+    plt.plot(x,width,color='r')#转发规模随时间变化
+    plt.savefig('Picture/weibo'+Wid+'Width.png')
+    plt.show()
+
+def RTWidthByTime(G,Wid):#一条微博信息的每个时刻（15min）的传播广度
+    width=['']*300
+    time = nx.get_node_attributes(G,'time')
+    for i in time:
+        if time[i]/900 < 288:
+            width[(time[i]/900)+1]=width[(time[i]/900)+1]+1
+    x=range(300)
+    plt.plot(x,width,color='r')#转发规模随时间变化
+    plt.savefig('Picture/weibo'+Wid+'AverageWidth.png')
+    plt.show()
+
+
+def RTDepth(G,mainNode,Wid):#一条微博信息每时刻的传播深度
+    depth=['']*300
+    time = nx.get_node_attributes(G,'time')
+    for i in time:
+        if time[i]/900 < 288:
+            depth[(time[i]/900)+1]=depth[(time[i]/900)+1]+i+'\001'
+    temp=[mainNode]
+    for i in range(depth.__len__()):
+        if depth[i]!=0 :
+            temp = temp+depth[i].split('\001')[:-1]
+            NG=MissInformation(G.subgraph(temp),mainNode)
+            depth[i]=nx.eccentricity(NG.to_undirected())[mainNode]
+    x=range(300)
+    plt.plot(x,depth,color='r')#转发规模随时间变化
+    plt.savefig('Picture/weibo'+Wid+'Depth.png')
+    plt.show()
+
 G = nx.DiGraph()
-width=[0]*300
-depth=['']*300
-with open('F:/github/WoLongBigData/DataManager/weibo3794305741726764.txt', 'r') as f:
+wid='3794305741726764'
+mainnode='2724513'
+with open('E:/data/PredictMicroblog/WoLongBigData/DataManager/weibo'+wid+'.txt', 'r') as f:
     for position, line in enumerate(f):
         t= line.strip().split('\001')
         G.add_node(t[2],time=int(t[3]),content=t[4])
         G.add_edge(t[1],t[2])
-G = MissInformation(G,'2724513')
-time = nx.get_node_attributes(G,'time')
-for i in time:
-    if time[i]/900 < 288:
-        width[(time[i]/900)+1]=width[(time[i]/900)+1]+1
-        depth[(time[i]/900)+1]=depth[(time[i]/900)+1]+i+'\001'
-for i in range(width.__len__()-1):
-    width[i+1] =width[i+1]+width[i]
-x=range(300)
-plt.plot(x,width,color='r')#转发规模随时间变化
-plt.savefig("Picture/weibo3794305741726764Width.png")
-plt.show()
-temp=['2724513']
-for i in range(depth.__len__()):
-    if depth[i]!=0 :
-        temp = temp+depth[i].split('\001')[:-1]
-        NG=MissInformation(G.subgraph(temp),'2724513')
-        depth[i]=nx.eccentricity(NG.to_undirected())['2724513']
-        print depth[i]
-plt.plot(x,depth,color='r')#转发深度随时间变化
-plt.savefig("Picture/weibo3794305741726764Depth.png")
-plt.show()
+G = MissInformation(G,mainnode)
+RTDepth(G,mainnode,wid)
+
+
+
+
 
